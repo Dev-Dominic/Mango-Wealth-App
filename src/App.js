9e205cc268d45042160e0c6d5b5c4fca5bf7326d
dineahcohen <dineahcohen1@gmail.com>
2021-05-08 12:38:32 -0500

    fixed styling for onboarding intro page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/auth/signUp';
 import Login from './pages/auth/login';
+import OnboardingIntro from './pages/onboarding/onboardingIntro';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/sign-up' component={SignUp} />
           <Route exact path='/sign-in' component={Login} />
+          <Route exact path='/onboarding-intro' component={OnboardingIntro} />
         </Switch>
       </div>
     </Router>
---
+++ src/pages/onboarding/onboardingIntro.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Grid from '@material-ui/core/Grid';
+import userQuestions from '../../assets/user-question.svg'
+
+const OnboardingWrapper = styled.div`
+background: var(--login-background);
+padding: 4rem 10rem 10rem 10rem;
+
+`;
+
+const FormContainer = styled.div`
+    display: flex;
+    width: 70vh;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 2rem;
+`;
+
+const OnboardingContainer = styled.div`
+    display: flex;
+    height: 60vh;
+    margin-top: 2rem;
+`;
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const Button = styled.button`
+    background: var(--secondary-blue);
+    width: 224px;
+    height: 45px;
+    text-align: center;
+
+    dislay: flex;
+    justify-content: center;
+    border: none;
+    border-radius: 6px;
+
+    font-size: 16px;
+    font-weight: 700;
+    color: #ffff;
+`;
+
+const OnboardingIntro = () => {
+    return (
+        <OnboardingWrapper>
+            <p className="onboarding-heading"> User Onboarding </p>
+            <OnboardingContainer>
+                <Grid item xs={6}>
+                    <img src={userQuestions} alt="user-question" />
+                </Grid>
+
+                <Grid item xs={6}>
+                    <FormContainer>
+                        <p className="landing-sub-text"> Please answer a few questions to assist with your recommendation </p>
+                        <ContentContainer>
+                            <p className="onboarding-subtext"> Section 1: Demographics</p>
+                            <p className="onboarding-subtext"> Section 2: Financial Information</p>
+                            <p className="onboarding-subtext"> Section3: Financial Product Preferences </p>
+                        </ContentContainer>
+                    </FormContainer>
+
+                    <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                        <Button> Continue </Button>
+                    </div>
+                </Grid>
+
+            </OnboardingContainer>
+        </OnboardingWrapper>
+    );
+}
+
+export default OnboardingIntro;
