b8f1e74775c22c658b53a6dc626c08abea61c74e
dineahcohen <dineahcohen1@gmail.com>
2021-05-14 00:05:18 -0500

    added skeleton for dashboard

---
+++ src/dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const Dashboard = () => {
+    return (
+        <p> DASHBOARD </p>
+    );
+}
+
+export default Dashboard;
