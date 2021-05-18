0c2d32353bfe8c3280702d15856e6aa0507939ad
dineahcohen <dineahcohen1@gmail.com>
2021-05-18 10:26:34 -0500

    fixed rendering issue

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,13 +3,6 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Tabs, Tab, AppBar } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import logo from '../assets/logo.svg';
-import SignUp from '../pages/auth/signUp';
-import Login from '../pages/auth/login';
-import About from '../pages/About';
-import Contact from '../pages/ContactUs';
-import Services from '../pages/Services';
-import Home from '../pages/Home';
 
 
 const NavBar = () => {
@@ -32,7 +25,6 @@ const NavBar = () => {
                 </AllTabs>
             </AppBar>
 
-            {value === 0 && <Home />}
         </>
     );
 }
